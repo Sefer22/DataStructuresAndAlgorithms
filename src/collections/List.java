6f15be68eb224756e2dcd3e0a0916cebf3069db5
Sefer22 <sfrs87997@gmail.com>
2024-08-12 00:23:04 +0400

    first

---
+++ src/collections/List.java
@@ -0,0 +1,13 @@
+package collections;
+
+import java.util.ArrayList;
+
+public class List {
+    public static void main(String[] args) {
+
+        int array[] = new int[3];
+
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+
+    }
+}
